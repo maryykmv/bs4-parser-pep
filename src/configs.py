a7fa6582cf788f49863d81f2d5eae9e65032054a
Mary K <wildcat.3333@yandex.ru>
2023-09-09 21:35:50 +0700

    changes review configs.py

--- src/configs.py
+++ src/configs.py
@@ -1,7 +1,11 @@
 import argparse
 import logging
 from logging.handlers import RotatingFileHandler
-from constants import BASE_DIR, DT_FORMAT, LOG_FORMAT
+
+from constants import (
+    DT_FORMAT, LOG_FORMAT, OUTPUT_FILE, OUTPUT_PRETTY,
+    LOG_DIR, LOG_FILE
+)
 
 
 def configure_argument_parser(available_modes):
@@ -20,16 +24,16 @@ def configure_argument_parser(available_modes):
     parser.add_argument(
         '-o',
         '--output',
-        choices=('pretty', 'file'),
+        choices=(OUTPUT_PRETTY, OUTPUT_FILE),
         help='Дополнительные способы вывода данных'
     )
     return parser
 
 
 def configure_logging():
-    log_dir = BASE_DIR / 'logs'
+    log_dir = LOG_DIR
     log_dir.mkdir(exist_ok=True)
-    log_file = log_dir / 'parser.log'
+    log_file = LOG_FILE
     rotating_handler = RotatingFileHandler(
         log_file, maxBytes=10 ** 6, backupCount=5
     )
