0b42aca74990a0dec434eb5ea0419d0e78b143f9
Mary K <wildcat.3333@yandex.ru>
2023-09-10 17:28:19 +0700

    changes review2 utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from requests import RequestException
 
-from constants import CODE_PAGES
+from constants import CODE_PAGES, FEATURE
 from exceptions import ParserFindTagException
 
 ERROR_PAGE = 'Возникла ошибка при загрузке страницы {url}'
@@ -13,20 +13,12 @@ def get_response(session, url):
         response = session.get(url)
         response.encoding = CODE_PAGES
         return response
-    except RequestException:
-        raise ConnectionError(ERROR_PAGE.format(url=url))
+    except RequestException as error:
+        raise ConnectionError(ERROR_PAGE.format(url=url, error=error))
 
 
 def get_soup(session, url):
-    try:
-        response = session.get(url)
-        response.encoding = CODE_PAGES
-        if response is None:
-            return
-        soup = BeautifulSoup(response.text, 'lxml')
-        return soup
-    except RequestException:
-        raise ConnectionError(ERROR_PAGE.format(url=url))
+    return BeautifulSoup(get_response(session, url).text, FEATURE)
 
 
 def find_tag(soup, tag, attrs=None):
